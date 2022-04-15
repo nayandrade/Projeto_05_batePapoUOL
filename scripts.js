ed34dedd32b84aaab6dceaea31a7af010355ae83
Nayara <nrdeandrade@gmail.com>
2022-04-14 21:53:10 -0300

    Validação de Login

--- scripts.js
+++ scripts.js
@@ -1,4 +1,8 @@
 let mensagens = []
+let nome;
+let para = "Todos"
+let texto;
+let tipo = "message"
 // let mensagensNovas = []
 
 function mostrarMenu() {
@@ -14,53 +18,115 @@ function esconderMenu() {
     console.log("sumiu");
 }
 
-function entrar() {
-    document.querySelector(".login").classList.add("esconder");
-    buscarMensagens();
-    console.log("entrou")
+function login() {
+    // let promise = axios("https://mock-api.driven.com.br/api/v6/uol/messages");
+    // promise.then(entrar);
+    // promise.catch(tratarFalha)
+    nome = document.querySelector(".usuario").value
+    console.log(nome)
+    let novoAcesso = {
+        name: nome
+    };
+    let promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", novoAcesso);
+    promise.then(buscarMensagens)
+    promise.catch(tratarFalha)
+}
+
+// function criarUsuario(resposta) {
+//     let status = resposta.status;
+//     console.log(resposta.status);
+//     if (status === 400) {
+
+//     } else {
+//         buscarMensagens()
+//     }           
+// }
+
+function outroNome() {
+    alert("Digite outro nome")
+    console.log("Digite outro nome")
+    document.querySelector("input").value = ""
+    document.querySelector("input").placeholder = "Digite outro nome..."
+
 }
 
 function sair() {
     window.location.reload()
     console.log("saiu");
-
 }
 
 function buscarMensagens() {
-    const promise = axios("https://mock-api.driven.com.br/api/v6/uol/messages");
+    let promise = axios("https://mock-api.driven.com.br/api/v6/uol/messages");
     promise.then(carregarMensagens);
     promise.catch(tratarFalha);
 }
 
 function carregarMensagens(response) {
     mensagens = response.data;
-    // console.log(response.status);
+    //console.log(response.status);
     // console.log (mensagens)
     renderizarMensagens()
 }
 
 function renderizarMensagens() {
+    document.querySelector(".login").classList.add("esconder");
     let mensagem = document.querySelector(".container")
 
     for (let i = 0; i < mensagens.length; i++) {
         if (mensagens[i].type === "message") {
             mensagem.innerHTML += `<div class="mensagem"><p class="cinza">(${mensagens[i].time})</p><p><strong>${mensagens[i].from}</strong> para <strong>${mensagens[i].to}:</strong> ${mensagens[i].text}</p></div>`
-            
+
         } else if (mensagens[i].type === "private_message") {
             mensagem.innerHTML += `<div class="mensagem-privada"><p class="cinza">(${mensagens[i].time})</p><p><strong>${mensagens[i].from}</strong> para <strong>${mensagens[i].to}:</strong> ${mensagens[i].text}</p></div>`
-            
+
         } else if (mensagens[i].type === "status") {
             mensagem.innerHTML += `<div class="status"><p class="cinza">(${mensagens[i].time})</p><p><strong>${mensagens[i].from}</strong> ${mensagens[i].text}</p></div>`
-            
-        }   
+
+        }
     }
     document.querySelector(".container").lastChild.scrollIntoView()
-    setInterval(buscarMensagens, 3000);
-    //setInterval(adicionarMensagens, 3000);
+    setInterval(buscarMensagens, 10000);
+    // LEMBRAR DE MUDAR O TEMPO DE RELOAD!!!!!
+    //setInterval(adicionarMensagens, 3000); 
 }
 
+setInterval(manterConexao, 5000)
+
+function manterConexao() {
+    console.log(nome)
+    let manterAcesso = {
+        name: nome
+    };
+
+    let promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/status", manterAcesso);
+    promise.then(console.log("Acesso Mantido"));
+
+}
+
+function enviarMensagem() {
+    texto = document.querySelector(".texto").value
+    console.log(texto)    
+
+    let mensagem = {
+        from: nome,
+        to: para,
+        text: texto,
+        type: tipo
+    };
+    console.log(mensagem)
+
+    let promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/messages", mensagem);
+    promise.then(apagarMensagem);
+
+}
+
+function apagarMensagem() {
+    document.querySelector(".texto").value = ""    
+}
+
+
 // function adicionarMensagens () {
-//     const promise = axios("https://mock-api.driven.com.br/api/v6/uol/messages");
+//     let promise = axios("https://mock-api.driven.com.br/api/v6/uol/messages");
 //     promise.then(carregarMensagensNovamente);
 //     console.log("vai adicionar")
 
@@ -73,10 +139,10 @@ function renderizarMensagens() {
 //         for (let i = mensagens.length; i < mensagensNovas.length; i++) {
 //             if (mensagens[i].type === "message") {
 //                 mensagem.innerHTML += `<div class="mensagem"><p class="cinza">(${mensagens[i].time})</p><p><strong>${mensagens[i].from}</strong> para <strong>${mensagens[i].to}:</strong> ${mensagens[i].text}</p></div>`
-                
+
 //             } else if (mensagens[i].type === "private_message") {
 //                 mensagem.innerHTML += `<div class="mensagem-privada"><p class="cinza">(${mensagens[i].time})</p><p><strong>${mensagens[i].from}</strong> para <strong>${mensagens[i].to}:</strong> ${mensagens[i].text}</p></div>`
-                
+
 //             } else if (mensagens[i].type === "status") {
 //                 mensagem.innerHTML += `<div class="status"><p class="cinza">(${mensagens[i].time})</p><p><strong>${mensagens[i].from}</strong> ${mensagens[i].text}</p></div>`
 //             }   
@@ -85,10 +151,13 @@ function renderizarMensagens() {
 //     }
 //     document.querySelector(".container").lastChild.scrollIntoView();
 //     setInterval(adicionarMensagens, 3000);
-     
+
 // }
 
 function tratarFalha(erroMensagens) {
-    const statusCode = erroMensagens.response.status;
+    let statusCode = erroMensagens.response.status;
     console.log(statusCode);
+    if (statusCode === 400) {
+        outroNome()
     }
+}
